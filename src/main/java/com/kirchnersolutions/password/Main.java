cf6196f9d11172b4378f5faa38ee748909e10041
Robert Kirchner JR <rjojjr91@live.com>
2019-11-12 10:29:51 -0600

    init

---
+++ src/main/java/com/kirchnersolutions/password/Main.java
@@ -0,0 +1,42 @@
+package com.kirchnersolutions.password;
+
+import com.kirchnersolutions.utilities.ByteTools;
+import com.kirchnersolutions.utilities.CryptTools;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String VERSION = "1.0.01a";
+    public static final String BUILD = "100101";
+
+    public static void main(String[] args) throws Exception{
+        Scanner in = new Scanner(System.in);
+        String option;
+        while(true){
+            System.out.println("Password and Time Generator");
+            System.out.println("Version: " + VERSION);
+            System.out.println("Build: " + BUILD);
+            System.out.println("1. Create Password");
+            System.out.println("2. Get Current Time");
+            System.out.println("exit. End Program");
+            option = in.next();
+            if(option.toLowerCase().equals("exit")){
+                System.out.println("Exiting program...");
+                System.exit(0);
+            }else if(option.equals("1")){
+                System.out.println("Enter passphrase");
+                String passphrase = in.next();
+                System.out.println("Password:");
+                System.out.println(CryptTools.generateEncodedSHA256Password(passphrase));
+                System.out.println("Enter c to continue");
+                option = in.next();
+            }else{
+                System.out.println("Current Time: " + System.currentTimeMillis());
+                System.out.println("Enter c to continue");
+                option = in.next();
+            }
+        }
+    }
+
+}
